8e41a36d92f62c149cc49415d96bceb113f58fd0
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-07-20 11:58:16 +0100

    register question routes

--- src/app.js
+++ src/app.js
@@ -5,14 +5,18 @@ const surveyRoutes = require("./routes/survey.routes")
 
 const sectionRoutes = require("./routes/section.routes")
 
+const questionRoutes = require("./routes/question.routes");
+
 const app = express()
 
 app.use(cors())
 
 app.use(express.json())
 
-app.use("/api/serveys", surveyRoutes)
+app.use("/api/surveys", surveyRoutes)
 
 app.use("/api/sections", sectionRoutes)
+
+app.use("/api/questions", questionRoutes);
 
 module.exports = app
